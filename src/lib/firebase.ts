47d69e259fdd1587f4871f89d7964224e74dca77
wendigozali93 <wendigozali93@guru.sd.belajar.id>
2026-07-12 22:08:33 +0700

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,7 +9,14 @@ import {
 } from "firebase/auth";
 import firebaseConfig from "../../firebase-applet-config.json";
 
-const app = initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyB9OnqGZ-gn9t67MJiwv817Ex3L_tRDdW4",
+  authDomain: "gen-lang-client-0194352979.firebaseapp.com",
+  projectId: "gen-lang-client-0194352979",
+  storageBucket: "gen-lang-client-0194352979.firebasestorage.app",
+  messagingSenderId: "269384836312",
+  appId: "1:269384836312:web:3e3509d2f93c75a468e3ad"
+};
 export const auth = getAuth(app);
 
 export const googleProvider = new GoogleAuthProvider();
