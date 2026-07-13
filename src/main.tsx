9f24f4b13c33192c3b7fa05a443a9e47b5b746b7
wendigozali93 <wendigozali93@guru.sd.belajar.id>
2026-07-13 10:41:02 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from "./App";
+
+import "./index.css";
+
+ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
